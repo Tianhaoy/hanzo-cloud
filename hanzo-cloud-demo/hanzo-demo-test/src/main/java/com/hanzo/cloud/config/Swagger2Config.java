ef3e37299dc9e5cd243a7f3084ab7ed15c4c9fa1
haoyu <2469653218@qq.com>
2020-09-15 11:54:04 +0800

    通过网关统一管理knife4j接口文档

---
+++ hanzo-cloud-demo/hanzo-demo-test/src/main/java/com/hanzo/cloud/config/Swagger2Config.java
@@ -0,0 +1,41 @@
+package com.hanzo.cloud.config;
+
+import com.github.xiaoymin.knife4j.spring.annotations.EnableKnife4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * @Author thy
+ * @Date 2020/9/13 23:30
+ * @Description:Swagger2增强版配置knife4j
+ */
+@Configuration
+@EnableSwagger2
+@EnableKnife4j
+public class Swagger2Config {
+    @Bean
+    public Docket createRestApi(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.hanzo.cloud.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("hanzo-knife4j-test")
+                .description("测试demo-API文档")
+                .contact("hanzo")
+                .version("1.0")
+                .build();
+    }
+}
--- hanzo-cloud-demo/hanzo-demo-test/src/main/java/com/hanzo/cloud/controller/test.java
+++ hanzo-cloud-demo/hanzo-demo-test/src/main/java/com/hanzo/cloud/controller/test.java
@@ -1,6 +1,8 @@
 package com.hanzo.cloud.controller;
 
 import com.alibaba.fastjson.JSONObject;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -13,13 +15,15 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @Slf4j
+@Api(value = "testController",description = "我是你爹controller")
 public class test {
 
+    @ApiOperation(value = "我是你爹",notes = "我是你爹")
     @RequestMapping(value = "/getFirst",method = RequestMethod.GET)
     public Object getFirst() throws Exception {
         JSONObject obj = new JSONObject();
         obj.put("code",200);
         log.info("get First...");
-        throw new Exception();
+        return obj;
     }
 }
