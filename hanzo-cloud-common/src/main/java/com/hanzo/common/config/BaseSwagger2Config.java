0c7ba295a0f63a0a44eec05bb05a8147f27a12e8
haoyu <2469653218@qq.com>
2020-09-21 14:25:17 +0800

    mybatisPlus与pageHelper冲突问题修复

--- hanzo-cloud-common/src/main/java/com/hanzo/common/config/BaseSwagger2Config.java
+++ hanzo-cloud-common/src/main/java/com/hanzo/common/config/BaseSwagger2Config.java
@@ -1,9 +1,7 @@
 package com.hanzo.common.config;
 
-import com.github.xiaoymin.knife4j.spring.annotations.EnableKnife4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -11,7 +9,6 @@ import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * @Author thy
