37758104bd2b228c372cc50b1c860ae4900c0d9e
haoyu <2469653218@qq.com>
2020-09-28 19:40:28 +0800

    fallback

--- hanzo-cloud-gateway/hanzo-gateway-server/src/main/java/com/hanzo/gateway/controller/FallbackController.java
+++ hanzo-cloud-gateway/hanzo-gateway-server/src/main/java/com/hanzo/gateway/controller/FallbackController.java
@@ -1,7 +1,9 @@
 package com.hanzo.gateway.controller;
 
 import com.hanzo.common.api.CommonResult;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class FallbackController {
 
     @GetMapping("/fallBack")
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public CommonResult fallBack() {
         return CommonResult.failed("Get Request fallback!");
     }
