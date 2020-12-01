9ee88119ca36cef57e17fc0b15ffe8829d25b7ef
haoyu <2469653218@qq.com>
2020-12-01 11:06:15 +0800

    开发文档

--- README.md
+++ README.md
@@ -1,8 +1,40 @@
 # hanzo-cloud
-**半藏微服务**是基于SpringCloud(Hoxton.SR5) + SpringBoot(2.3.0.RELEASE)的 SaaS型微服务后端脚手架。
-核心技术采用SpringCloud、SpringBoot、Nacos、Gateway、OpenFegin、Ribbon、Hystrix(后期会采用sentienl)、JWT Token、Mybatis Plus、Mysql、Redis、ElasticSearch、RabbitMQ、kafka、RocketMQ、Seata、FastDFS等主要框架和中间件。
+**半藏微服务**是基于Spring Cloud Alibaba(Hoxton.SR5) + SpringBoot(2.3.0.RELEASE)的 SaaS型微服务后端脚手架。
 希望能努力打造一套 SaaS基础框架 - 分布式微服务架构 - 分布式XXL-Job - 持续集成 - 自动化部署K8s - 系统监测 的解决方案。 
-具备注册中心、网关、授权中心、消息驱动、消息队列、搜索引擎、链式追踪、分布式事务、分布式定时任务等多个模块。
+
+### 功能特点
+- 主体框架：采用SpringCloud、SpringBoot、Nacos、Gateway、OpenFegin、Ribbon、Hystrix(后期会采用sentienl)、JWT Token、Mybatis Plus、Mysql、Redis、ElasticSearch、RabbitMQ、kafka、RocketMQ、Seata、FastDFS等主要框架和中间件
+
+- 统一注册：采用Nacos作为注册中心
+
+- 统一认证：统一Oauth2认证协议，采用jwt的方式，实现统一认证
+
+- 业务监控：利用Spring Boot Admin 来监控各个独立Service的运行状态
+
+- 内部调用：集成了openfeign
+
+- 消息驱动：stream
+
+- 业务熔断：采用Sentinel实现业务熔断处理，避免服务之间出现雪崩(目前使用的hystrix)
+
+- 在线文档：通过接入knife4j，实现在线API文档的查看与调试;
+
+- 代码生成：基于Mybatis-plus-generator自动生成代码，提升开发效率，选择数据源，选择表结构，一键生成后端代码。
+
+- 消息中心：集成消息中间件kafka、RabbitMQ、RocketMQ，对业务进行异步处理;
+
+- 业务分离：采用前后端分离的框架设计，前端打算采用vue-element-admin（我不会前端，希望能有前端大佬一起合作开发，~~~~(>_<)~~~~）
+
+- 链式追踪：
+
+- 对象存储：OSS
+
+- 分布式定时任务：采用xxl-job中间件进行任务调度
+
+- 搜索引擎：采用elasticsearch搜索引擎实现搜索功能。
+
+- 自动化运维：docker,Jenkins,K8s
+
 
 ### 开发中
     hanzo-gateway-server 网关
@@ -13,7 +45,7 @@
     这五个模块开发完毕
     启动gateway center system monitor这四个模块即可 
     可以访问knif4j接口文档查看如何登录 或者入微信群
-
+    启动教程地址：
 ## 没时间写如何登录 过段时间会写文档
 
 ### 后端技术
@@ -43,3 +75,33 @@
 | Portainer                     | 可视化Docker容器管理  |
 | Jenkins                       | 自动化部署工具        |
 | K8s                           | 自动化部署           |
+
+### 文件结构
+```lua
+hanzo-cloud -- 父项目,各模块分离，方便集成和微服务
+│  ├─hanzo-cloud-admin -- 后台管理父模块
+│  │  ├─hanzo-cloud-system -- 系统管理子模块[6401]
+│  │─hanzo-cloud-auth -- 统一认证父模块 
+│  │  ├─hanzo-auth-center -- 授权中心子模块[6301]
+│  │  ├─hanzo-auth-client -- 授权客户端client公共jar包模块
+│  │─hanzo-cloud-common -- 公共jar包模块
+│  │─hanzo-cloud-demo -- 测试父模块
+│  │  ├─hanzo-demo-test -- 测试子模块[6001]
+│  │─hanzo-cloud-file -- 分布式文件父模块
+│  │─hanzo-cloud-gateway -- 网关父模块
+│  │  ├─hanzo-gateway-server -- gateway网关服务模块[6201]
+│  │  ├─hanzo-zuul-server -- zuul网关服务模块 作废
+│  │─hanzo-cloud-job -- xxl-job任务父模块
+│  │─hanzo-cloud-log -- log日志父模块
+│  │─hanzo-cloud-monitor -- 系统监控模块[6101]
+│  │─hanzo-cloud-msg -- 短信模块
+│  │─hanzo-cloud-queue -- 消息队列父模块
+│  │  ├─hanzo-cloud-kafka -- kafka消息队列子模块
+│  │  ├─hanzo-cloud-rabbitmq -- rabbitmq消息队列子模块
+│  │  ├─hanzo-cloud-rocketmq -- rocketmq消息队列子模块
+│  │─hanzo-cloud-search -- 搜索父模块
+│  │  ├─hanzo-cloud-elasticsearch -- es搜索引擎子模块
+│  │─hanzo-cloud-starter -- starter公共父模块
+│  │  ├─hanzo-starter-kafka -- kafka-starter子模块
+```
+
---
+++ doc/sql/hanzo_cloud.sql
@@ -0,0 +1,255 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : mysql8.0 root
+Source Server Version : 80018
+Source Host           : localhost:3306
+Source Database       : hanzo_cloud
+
+Target Server Type    : MYSQL
+Target Server Version : 80018
+File Encoding         : 65001
+
+Date: 2020-12-01 10:17:16
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for oauth_client_details
+-- ----------------------------
+DROP TABLE IF EXISTS `oauth_client_details`;
+CREATE TABLE `oauth_client_details` (
+  `client_id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '客户端ID',
+  `resource_ids` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT '资源id集合',
+  `client_secret` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '客户端(client)的访问密匙',
+  `scope` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '客户端申请的权限范围',
+  `authorized_grant_types` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT 'authorization_code,password,refresh_token,implicit,client_credentials',
+  `web_server_redirect_uri` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT '客户端的重定向URI',
+  `authorities` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT '用户的权限范围',
+  `access_token_validity` int(11) DEFAULT NULL COMMENT 'access_token的有效时间',
+  `refresh_token_validity` int(11) DEFAULT NULL COMMENT 'refresh_token有效期(秒)',
+  `additional_information` varchar(4096) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT '预留字段，值必须是json格式',
+  `autoapprove` tinyint(4) DEFAULT NULL COMMENT '默认false,适用于authorization_code模式,设置用户是否自动approval操作,设置true跳过用户确认授权操作页面，直接跳到redirect_uri',
+  `origin_secret` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT '拓展字段-初始密钥',
+  PRIMARY KEY (`client_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='客户端配置表';
+
+-- ----------------------------
+-- Records of oauth_client_details
+-- ----------------------------
+INSERT INTO `oauth_client_details` VALUES ('app', '', '$2a$10$8Qk/efslEpO1Af1kyw/rp.DdJGsdnET8UCp1vGDzpQEa.1qBklvua', 'all', 'refresh_token,password', '', null, '14400', '14400', null, null, '123456');
+INSERT INTO `oauth_client_details` VALUES ('hanzo', ' ', '$2a$10$aSZTvMOtUAYUQ.75z2n3ceJd6dCIk9Vy3J/SKZUE4hBLd6sz7.6ge', 'all', 'password,refresh_token', null, null, '14400', '14400', null, '0', '123456');
+
+-- ----------------------------
+-- Table structure for sys_client
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_client`;
+CREATE TABLE `sys_client` (
+  `c_id` int(11) NOT NULL AUTO_INCREMENT COMMENT '子服务器id',
+  `c_name_eng` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '子服务器英文名',
+  `c_name_cn` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '子服务器中文名',
+  `c_detail` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '子服务器描述',
+  `c_secret` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '子服务器秘钥',
+  `c_lock` int(11) DEFAULT NULL COMMENT '子服务器是否上线 0未上线/1已上线',
+  PRIMARY KEY (`c_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=38 DEFAULT CHARSET=utf8 COMMENT='测试数据表 无用的';
+
+-- ----------------------------
+-- Records of sys_client
+-- ----------------------------
+INSERT INTO `sys_client` VALUES ('7', 'elasticsearch', 'es搜索', '测试', '123', '1');
+
+-- ----------------------------
+-- Table structure for sys_dept
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_dept`;
+CREATE TABLE `sys_dept` (
+  `dept_id` int(20) NOT NULL AUTO_INCREMENT COMMENT '部门ID',
+  `parent_id` int(20) NOT NULL COMMENT '上级部门ID',
+  `dept_name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '部门名称',
+  `order_num` double(20,0) DEFAULT NULL COMMENT '排序',
+  `create_time` datetime NOT NULL COMMENT '创建时间',
+  `modify_time` datetime DEFAULT NULL COMMENT '修改时间',
+  PRIMARY KEY (`dept_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci ROW_FORMAT=DYNAMIC COMMENT='部门表';
+
+-- ----------------------------
+-- Records of sys_dept
+-- ----------------------------
+INSERT INTO `sys_dept` VALUES ('1', '0', '半藏有限公司', '1', '2020-10-12 14:32:51', null);
+INSERT INTO `sys_dept` VALUES ('2', '1', '开发部', '2', '2020-10-12 14:33:34', null);
+INSERT INTO `sys_dept` VALUES ('3', '1', '人事部', '1', '2020-10-12 14:34:04', null);
+INSERT INTO `sys_dept` VALUES ('4', '2', '开发一部', '1', '2020-10-12 14:34:20', null);
+INSERT INTO `sys_dept` VALUES ('5', '1', '开发二部', '2', '2020-10-18 14:20:37', null);
+
+-- ----------------------------
+-- Table structure for sys_log
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_log`;
+CREATE TABLE `sys_log` (
+  `log_id` int(20) NOT NULL AUTO_INCREMENT COMMENT '日志ID',
+  `username` varchar(50) DEFAULT NULL COMMENT '操作用户',
+  `operation` text COMMENT '操作内容',
+  `time` decimal(11,0) DEFAULT NULL COMMENT '耗时',
+  `method` text COMMENT '操作方法',
+  `params` text COMMENT '方法参数',
+  `ip` varchar(64) DEFAULT NULL COMMENT '操作者IP',
+  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+  `location` varchar(50) DEFAULT NULL COMMENT '操作地点',
+  PRIMARY KEY (`log_id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC COMMENT='用户操作日志表';
+
+-- ----------------------------
+-- Records of sys_log
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for sys_login_log
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_login_log`;
+CREATE TABLE `sys_login_log` (
+  `log_id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `username` varchar(50) NOT NULL COMMENT '用户名',
+  `login_time` datetime NOT NULL COMMENT '登录时间',
+  `location` varchar(50) DEFAULT NULL COMMENT '登录地点',
+  `ip` varchar(50) DEFAULT NULL COMMENT 'IP地址',
+  `system` varchar(50) DEFAULT NULL COMMENT '操作系统',
+  `browser` varchar(50) DEFAULT NULL COMMENT '浏览器',
+  PRIMARY KEY (`log_id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC COMMENT='登录日志表';
+
+-- ----------------------------
+-- Records of sys_login_log
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for sys_menu
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_menu`;
+CREATE TABLE `sys_menu` (
+  `menu_id` int(20) NOT NULL AUTO_INCREMENT COMMENT '菜单/按钮ID',
+  `parent_id` int(20) NOT NULL COMMENT '上级菜单ID',
+  `menu_name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '菜单/按钮名称',
+  `path` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '对应路由path',
+  `component` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '对应路由组件component',
+  `perms` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '权限标识',
+  `icon` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '图标',
+  `type` char(2) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '类型 0菜单 1按钮',
+  `order_num` double(20,0) DEFAULT NULL COMMENT '排序',
+  `create_time` datetime NOT NULL COMMENT '创建时间',
+  `modify_time` datetime DEFAULT NULL COMMENT '修改时间',
+  PRIMARY KEY (`menu_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci ROW_FORMAT=DYNAMIC COMMENT='菜单表';
+
+-- ----------------------------
+-- Records of sys_menu
+-- ----------------------------
+INSERT INTO `sys_menu` VALUES ('1', '0', '系统管理1', '/system', '', '', '', '0', '1', '2020-10-26 00:03:32', '2020-10-26 00:13:12');
+INSERT INTO `sys_menu` VALUES ('3', '0', '系统监控', '/moniter', '', '', '', '0', '2', '2020-10-26 00:57:15', null);
+INSERT INTO `sys_menu` VALUES ('4', '1', '用户管理', '/system/user', '', '', '', '0', '1', '2020-10-26 00:57:51', null);
+INSERT INTO `sys_menu` VALUES ('5', '3', '登录日志', '/monitor/loginlog', '', '', '', '0', '1', '2020-10-26 00:58:35', null);
+INSERT INTO `sys_menu` VALUES ('7', '4', '新增用户', '/user/addUser', null, 'user:add', null, '1', '1', '2020-11-05 01:11:58', null);
+INSERT INTO `sys_menu` VALUES ('8', '4', '查询用户', '/user/getUserList', null, 'user:list', null, '1', '1', '2020-11-08 01:09:40', null);
+
+-- ----------------------------
+-- Table structure for sys_role
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_role`;
+CREATE TABLE `sys_role` (
+  `role_id` int(20) NOT NULL AUTO_INCREMENT COMMENT '角色ID',
+  `role_name` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '角色名称',
+  `remake` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '角色描述',
+  `create_time` datetime NOT NULL COMMENT '创建时间',
+  `modify_time` datetime DEFAULT NULL COMMENT '修改时间',
+  PRIMARY KEY (`role_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci ROW_FORMAT=DYNAMIC COMMENT='角色表';
+
+-- ----------------------------
+-- Records of sys_role
+-- ----------------------------
+INSERT INTO `sys_role` VALUES ('1', '管理员', '管理员', '2020-10-12 14:08:46', null);
+INSERT INTO `sys_role` VALUES ('2', '管理员2', '管理员2', '2020-10-17 01:37:34', null);
+INSERT INTO `sys_role` VALUES ('3', '注册用户', '可查看，新增，导出，不可修改，删除', '2020-11-22 23:22:49', null);
+
+-- ----------------------------
+-- Table structure for sys_role_menu
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_role_menu`;
+CREATE TABLE `sys_role_menu` (
+  `role_id` int(20) NOT NULL COMMENT '角色ID',
+  `menu_id` int(20) NOT NULL COMMENT '菜单/按钮ID'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci ROW_FORMAT=DYNAMIC COMMENT='角色菜单关联表';
+
+-- ----------------------------
+-- Records of sys_role_menu
+-- ----------------------------
+INSERT INTO `sys_role_menu` VALUES ('1', '7');
+INSERT INTO `sys_role_menu` VALUES ('1', '8');
+
+-- ----------------------------
+-- Table structure for sys_social_user
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_social_user`;
+CREATE TABLE `sys_social_user` (
+  `username` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT 'hanzo系统用户名',
+  `social_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '第三方平台名称',
+  `social_uuid` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '第三方平台账户ID',
+  `social_user_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '第三方平台用户名',
+  `social_nick_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '第三方平台昵称',
+  `social_image_url` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '第三方平台头像',
+  `location` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '地址',
+  `remake` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '备注',
+  PRIMARY KEY (`username`,`social_name`,`social_uuid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='第三方登录与系统内部用户绑定表';
+
+-- ----------------------------
+-- Records of sys_social_user
+-- ----------------------------
+INSERT INTO `sys_social_user` VALUES ('hanzo-test', 'GITHUB', '45312497', 'Tianhaoy', '', null, null, null);
+
+-- ----------------------------
+-- Table structure for sys_user
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_user`;
+CREATE TABLE `sys_user` (
+  `user_id` int(20) NOT NULL AUTO_INCREMENT COMMENT '用户ID',
+  `username` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '用户名',
+  `password` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '密码',
+  `dept_id` int(20) DEFAULT NULL COMMENT '部门ID',
+  `email` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '邮箱',
+  `mobile` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '联系电话',
+  `status` char(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '状态 0锁定 1有效',
+  `create_time` datetime NOT NULL COMMENT '创建时间',
+  `modify_time` datetime DEFAULT NULL COMMENT '修改时间',
+  `last_login_time` datetime DEFAULT NULL COMMENT '最近访问时间',
+  `sex` char(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '性别 0男 1女 2保密',
+  `is_tab` char(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '是否开启tab，0关闭 1开启',
+  `theme` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '主题',
+  `avatar` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '头像',
+  `description` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '描述',
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci ROW_FORMAT=DYNAMIC COMMENT='用户表';
+
+-- ----------------------------
+-- Records of sys_user
+-- ----------------------------
+INSERT INTO `sys_user` VALUES ('1', 'hanzo-test', '$2a$10$bHzGQ71kzDVGTR0Wl8HsGeQNVcs9kCgi858DlpFMrCIn1SyLcoesC', '3', '2469653218@qq.com', '13578844781', '1', '2020-10-12 00:30:59', '2020-10-17 02:23:16', null, '2', '0', null, null, 'hanzo-test');
+INSERT INTO `sys_user` VALUES ('10', '江疏影', '$2a$10$bHzGQ71kzDVGTR0Wl8HsGeQNVcs9kCgi858DlpFMrCIn1SyLcoesC', '1', '2469653218@qq.com', '18899997777', '1', '2020-10-18 02:12:39', null, null, '2', null, null, null, null);
+
+-- ----------------------------
+-- Table structure for sys_user_role
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_user_role`;
+CREATE TABLE `sys_user_role` (
+  `user_id` int(20) NOT NULL COMMENT '用户ID',
+  `role_id` int(20) NOT NULL COMMENT '角色ID'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci ROW_FORMAT=DYNAMIC COMMENT='用户角色关联表';
+
+-- ----------------------------
+-- Records of sys_user_role
+-- ----------------------------
+INSERT INTO `sys_user_role` VALUES ('1', '1');
+INSERT INTO `sys_user_role` VALUES ('1', '2');
+INSERT INTO `sys_user_role` VALUES ('10', '1');
+INSERT INTO `sys_user_role` VALUES ('10', '2');
